62050cd6851fd25e175179245f754462c2d00744
*GanivadaShiva* <*shiva16140.gmail.com*>
2023-08-14 22:00:00 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react"
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
-/*import About from "./components/About";*/
+import Alert from "./components/Alert";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   const [mode,setMode]=useState('light');
@@ -9,18 +12,39 @@ function App() {
     if (mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='#042743';
+      showAlert("Dark Mode Enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light Mode Enabled","success");
     }
   }
+  const[alert,setalert]=useState(null);
+  const showAlert = ( message ,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
   return (
-    <div>
-      <NavBar title="RGUKT" mode={mode} toggleMode={toggleMode}/>
-      <TextForm heading="Enter Text" mode={mode}/>
-      {/*<About/>*/}
+    <Router>  
+    <NavBar title="RGUKT" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+      <Route exact path="/about">
+      <About mode={mode}/>
+      </Route>
+      <Route exact path="/">
+      <TextForm heading="Enter Text" mode={mode} showAlert={showAlert}/>
+      </Route>
+    </Switch>
     </div>
+    </Router>
   );
 }
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,6 +5,7 @@ export default function TextForm(props){
     const handleUpperCase=()=>{
         let newtext=text.toLocaleUpperCase();
         setText(newtext)
+        props.showAlert("Converted to  uppercase","success");
     }
     const handleChange=(event)=>{
         setText(event.target.value)
@@ -12,23 +13,25 @@ export default function TextForm(props){
     const handleLoCase=()=>{
         let newtext=text.toLocaleLowerCase();
         setText(newtext)
+        props.showAlert("Converted to  lowercase","success");
     }
     const handleClText=()=>{
         setText("");
+        props.showAlert("Text cleared","success");
     }
     return(
         <>
 <div className=" container mb-3" style={{color:props.mode==='light'?'black':'white'}}>
   <h2>{props.heading}</h2>
-  <textarea className="form-control" id="Box" rows="5" style={{backgroundColor:props.mode==='light'?'white':'grey',color:props.mode==='light'?'dark':'white'}}value={text} onChange={handleChange}></textarea><br/>
-  <button className="btn btn-primary mx-2" onClick={handleUpperCase}>Upper Case</button>
-  <button className="btn btn-primary mx-2" onClick={handleLoCase}>Lower Case</button>
-  <button className="btn btn-primary mx-2" onClick={handleClText}>Clear Text</button>
+  <textarea className="form-control" id="Box" rows="7" style={{backgroundColor:props.mode==='light'?'white':'#13466e',color:props.mode==='light'?'black':'white'}}value={text} onChange={handleChange}></textarea><br/>
+  <button disabled={text.length===0}className="btn btn-primary mx-2" onClick={handleUpperCase}>Upper Case</button>
+  <button disabled={text.length===0}className="btn btn-primary mx-2" onClick={handleLoCase}>Lower Case</button>
+  <button disabled={text.length===0}className="btn btn-primary mx-2" onClick={handleClText}>Clear Text</button>
 </div>
 <div className="container" style={{color:props.mode==='light'?'black':'white'}}>
     <h2>Your text summery</h2>
-    <p>No.of word:- {text.split(" ").length} and No.of characters:- {text.length}</p>
-    <p>Time to read:- {0.08 * text.split(" ").length}</p>
+    <p>Words = {text.split(/\s+/).filter((e) => e.length !== 0).length} And Characters = {text.length}</p>
+    <p>Time to read:- {0.08 * text.split(" ").filter( (e) => { return e.length !==0}).length}</p>
     <h2>Preview</h2>
     <p>{text.length>0?text:"Nothing to preview"}</p>
 </div>
